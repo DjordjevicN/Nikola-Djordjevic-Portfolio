eeb907572b407a2e71d5425e93bd1c4752db77d5
nikola djordjevic <nikola.dj.87@gmail.com>
2019-10-27 00:53:09 +0200

    adding project

--- animation.js
+++ animation.js
@@ -151,7 +151,23 @@ buttonWork.addEventListener('click', () => {
                 <div class="gallery-grid">
 
 
-
+                <div class="gallery-box">
+                <h1 class="work-title">Real Estate</h1>
+                <!-- link to PAGE vv-->
+                <a href="https://djordjevicn.github.io/RealEstate-1/" target="_blank"><img
+                        src="https://raw.githubusercontent.com/DjordjevicN/RealEstate-1/master/Screenshot_1.jpg"
+                        alt="project"></a>
+                <!-- link to PIC ^^-->
+                <div class="links">
+                    <!-- link to GITHUB vv  -->
+                    <a href="https://github.com/DjordjevicN/RealEstate-1" target="_blank"> <i
+                            class="fab fa-github"></i></a>
+                    <!-- link to PAGE -->
+                    <a href="https://djordjevicn.github.io/RealEstate-1/" target="_blank">
+                        <i class="fas fa-globe"></i>
+                    </a>
+                </div>
+            </div>
 
 
                     <div class="gallery-box">
